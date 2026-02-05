1625c7f277346c1d0353f6f2a2127425168c9757
Zhongmin Ma <ma65536@gmail.com>
2026-02-04 20:50:26 -0500

    deploy and run gcloud

---
+++ python-app/README.md
@@ -0,0 +1,3 @@
+gcloud builds submit . `
+  --config=manual-deploy-run.yaml `
+  --service-account=projects/gcp-devops-ma2026/serviceAccounts/gcp-devops@gcp-devops-ma2026.iam.gserviceaccount.com
--- python-app/app.py
+++ python-app/app.py
@@ -3,7 +3,7 @@ app = flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from python in docker container"
+    return "<h1>Hello from python in docker container</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
